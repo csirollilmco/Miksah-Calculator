fd20ae659ad437934327f6fb8a4feef2f116e2b0
Christian Sirolli <christian.a.sirolli@lmco.com>
2021-09-29 15:32:26 +0000

    Update package.json, LICENSE, packages/src-renderer/package.json, packages/src-renderer/index.html, packages/src-renderer/.eslintrc.js, packages/src-renderer/src/main.js

--- packages/src-renderer/.eslintrc.js
+++ packages/src-renderer/.eslintrc.js
@@ -5,15 +5,6 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'standard',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser'
-  },
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off'
-  }
+    'standard'
+  ]
 }
