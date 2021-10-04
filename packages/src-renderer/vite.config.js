cfba8de0e1005c21264a7c9def3ad1f7881d0bbe
csirollilmco <91908164+csirollilmco@users.noreply.github.com>
2021-10-04 16:56:09 -0400

    Update vite.config.js

--- packages/src-renderer/vite.config.js
+++ packages/src-renderer/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -8,5 +8,10 @@ export default defineConfig({
       '~': resolve(__dirname, './'),
       '@src': resolve(__dirname, 'src')
     }
-  }
+  },
+  plugins: [
+		svelte({
+			/* plugin options */
+		})
+	]
 })
